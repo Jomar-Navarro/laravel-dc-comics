0299f7aa29fbbb674a64e6c711d5baa2f26946d2
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-16 16:20:21 +0200

    [add] style

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\ComicsController;
 
 
 /*
@@ -19,5 +20,6 @@ Route::get('/', [PageController::class, 'index'])->name('home');
 
 Route::get('/nuova-pagina', [PageController::class, 'nuovaPagina'])->name('nuova-pagina');
 
+Route::resource('comics', ComicsController::class);
 
 
